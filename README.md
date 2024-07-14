04405248084ba59658a485f0f19af37abc0ed5bf
Esha <maheshwariesha11@gmail.com>
2024-07-14 11:52:25 +0530

    added README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+## CSI_6th_Assignment(NodeJs)
+### "RESTful API using Node.js and Express that supports basic CRUD operations on Products" 
+
+
+![createProduct](images/createProduct.png)
+
+
+![getAllProducts](images/getAllProducts.png)
+
+
+![getProductById](images/getProductById.png)
+
+
+![updateProduct](images/updateProduct.png)
+
+
+![MongoDB](images/MongoDB.png)
+
+
+![deleteProduct](images/deleteProduct.png)
--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const PORT = process.env.PORT;
-const MONGODB_URL = process.env.MONGODB_URL;
+const ApiRoutes = require('./routes/product');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/api/product', ApiRoutes);
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URL);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Connected to the database.")
     } catch (error) {
-        console.log("Error in connecting to MongoDB", error);
+        console.log(error.message)
     }
 }
 
-const setUpAndStartServer = async () => {
-    app.listen(PORT, async () => {
-        console.log(`Server running at PORT: ${PORT}`)
+const SetUpAndStartServer = async () => {
+    app.listen(process.env.PORT, async () => {
+        console.log(`Server started on Port: ${process.env.PORT}`);
         await connectToDB();
     });
 }
 
-setUpAndStartServer();
+SetUpAndStartServer();
